4ea7201fb14b1b63a80d6b7ee444901fbc094c94
Taylor Walker <taylor.walker@hotmail.com>
2019-01-18 16:22:25 -0800

    resave file

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var cheerio = require("cheerio");
 
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = 8080;
 
 var app = express();
 
@@ -21,76 +21,87 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 app.get("/scrape", function (req, res) {
-  axios.get("http://weeklyworldnews.com/").then(function (response) {
+  // axios.get("http://weeklyworldnews.com/").then(function (response) {
 
+  //   var $ = cheerio.load(response.data);
+
+  //   // An empty array to save data
+  //   var results = [];
+
+  //   $("article h1").each(function (i, element) {
+  //save text in "title"
+  // var title = $(element).text();
+  // //save "href" attributes in "link"
+  // var link = $(element).children("a").attr("href");
+
+  //     //if (title && link) {
+  //     //};
+
+  //     // Save results in empty array
+  //     results.push({
+  //       title: title,
+  //       link: link
+  //     });
+  //   });
+  axios.get("http://weeklyworldnews.com/").then(function (response) {
     var $ = cheerio.load(response.data);
 
-    // An empty array to save data
-    var results = [];
-
     $("article h1").each(function (i, element) {
-      //save text in "title"
-      var title = $(element).text();
-      //save "href" attributes in "link"
-      var link = $(element).children("a").attr("href");
-
-      //if (title && link) {
-      //};
-
-      // Save results in empty array
-      results.push({
-        title: title,
-        link: link
-      });
+      result.title = $(this).text();
+      result.link = $(this).children("a").attr("href");
+      db.Article.create(result)
+        .then(function (dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
     });
 
-    // Log the results once you've looped through each of the elements found with cheerio
-    console.log(results);
+    // Send a message to the client
     res.send("Scrape Complete");
   });
 
-
-
+  // Log the results once you've looped through each of the elements found with cheerio
+  console.log(results);
+  res.send("Scrape Complete");
 });
 
-app.get("/articles", function(req, res) {
+app.get("/articles", function (req, res) {
   db.Article.find({})
-    .then(function(dbArticle) {
+    .then(function (dbArticle) {
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       res.json(err);
     });
 });
 
-
-app.get("/articles/:id", function(req, res) {
+app.get("/articles/:id", function (req, res) {
   db.Article.findOne({ _id: req.params.id })
     .populate("note")
-    .then(function(dbArticle) {
+    .then(function (dbArticle) {
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       res.json(err);
     });
 });
 
-
-app.post("/articles/:id", function(req, res) {
+app.post("/articles/:id", function (req, res) {
   db.Note.create(req.body)
-    .then(function(dbNote) {
+    .then(function (dbNote) {
       return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
     })
-    .then(function(dbArticle) {
+    .then(function (dbArticle) {
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       res.json(err);
     });
 });
 
-// Start the server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App running on port " + PORT + "!");
 });
 
