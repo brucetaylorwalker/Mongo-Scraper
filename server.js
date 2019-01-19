4b64e222121b0a69fa6f798f826b4590b9fba719
Taylor Walker <taylor.walker@hotmail.com>
2019-01-18 18:17:55 -0800

    commit/scrape not working

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var cheerio = require("cheerio");
 
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = 8080;
 
 var app = express();
 
@@ -22,52 +22,63 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 app.get("/scrape", function (req, res) {
   axios.get("http://weeklyworldnews.com/").then(function (response) {
-
     var $ = cheerio.load(response.data);
 
-    // An empty array to save data
-    var results = [];
-
     $("article h1").each(function (i, element) {
-  // save text in "title"
-  var title = $(element).text();
-  //save "href" attributes in "link"
-  var link = $(element).children("a").attr("href");
+      // An empty array to save data
+      var result = [];
+      // save text in "title"
+      // var title = $(element).text();
+      result.title = $(this).text();
+      //save "href" attributes in "link"
+      // var link = $(element).children("a").attr("href");
+      result.link = $(this).children("a").attr("href");
 
-  //     //if (title && link) {
-  //     //};
+      db.Article.create(result)
+        .then(function (dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function (err) {
+          // If an error occurred, log it
+          console.log(err);
+        });
+      //     //if (title && link) {
+      //     //};
 
       // Save results in empty array
-      results.push({
-        title: title,
-        link: link
-      });
+      // results.push({
+      //   title: title,
+      //   link: link
     });
-  // axios.get("http://weeklyworldnews.com/").then(function (response) {
-  //   var $ = cheerio.load(response.data);
-
-  //   $("article h1").each(function (i, element) {
-
-  //     var result = {};
-
-  //     result.title = $(this).text();
-  //     result.link = $(this).children("a").attr("href");
-  //     db.Article.create(result)
-  //       .then(function (dbArticle) {
-  //         console.log(dbArticle);
-  //       })
-  //       .catch(function (err) {
-  //         console.log(err);
-  //       });
-  //   });
-
- 
+    res.send("Scrape Complete");
   });
-
-  // Log the results once you've looped through each of the elements found with cheerio
-  console.log(results);
-  res.send("Scrape Complete");
 });
+// axios.get("http://weeklyworldnews.com/").then(function (response) {
+//   var $ = cheerio.load(response.data);
+
+//   $("article h1").each(function (i, element) {
+
+//     var result = {};
+
+//     result.title = $(this).text();
+//     result.link = $(this).children("a").attr("href");
+//     db.Article.create(result)
+//       .then(function (dbArticle) {
+//         console.log(dbArticle);
+//       })
+//       .catch(function (err) {
+//         console.log(err);
+//       });
+//   });
+
+
+
+
+// Log the results once you've looped through each of the elements found with cheerio
+// // console.log(results);
+// res.send("Scrape Complete");
+
 
 app.get("/articles", function (req, res) {
   db.Article.find({})
